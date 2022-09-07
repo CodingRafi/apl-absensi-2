b869c0e6ad009c281d414d754a6674d936da8fca
dwinrynt <dwinuryantosukatanai@gmail.com>
2022-09-07 08:51:09 +0700

    pull

--- app/Http/Controllers/TahunAjaranController.php
+++ app/Http/Controllers/TahunAjaranController.php
@@ -62,7 +62,7 @@ class TahunAjaranController extends Controller
             'sekolah' => \Auth::user()->sekolah,
         ]);
 
-        return redirect('/tahun-ajaran')->with('Berhasil Menambahkan Tahun Ajaran Baru');
+        return redirect('/tahun-ajaran')->with('message', 'Berhasil Menambahkan Tahun Ajaran Baru');
     }
 
     /**
@@ -111,7 +111,7 @@ class TahunAjaranController extends Controller
             'status' => $request->status,
         ]);
 
-        return redirect('/tahun-ajaran')->with('Berhasil Di Update');
+        return redirect('/tahun-ajaran')->with('message', 'Berhasil Di Update');
     }
 
     /**
